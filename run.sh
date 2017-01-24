62727904069361ee2ac3d9dd3415ec6163f524ae
Louis Chavane <louis.chavane@gmail.com>
2017-01-24 15:51:41 +0100

    change phantomjs url

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-wget https://github.com/paladox/phantomjs/releases/download/2.1.7/phantomjs-2.1.1-linux-x86_64.tar.bz2
+wget https://github.com/louischavane/phantomjs/raw/master/phantomjs-2.1.1-linux-x86_64.tar.bz2
 tar xfj phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo cp /tmp/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
